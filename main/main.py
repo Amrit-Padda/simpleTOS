f2cd6e0bbbd21a3b041a4f239f28cfd1df8d74cf
Jules Petit <jules.laptop@gmail.com>
2020-01-25 15:42:56 -0500

    Added gitIgnore

--- main/main.py
+++ main/main.py
@@ -4,16 +4,17 @@ import os
 #Import our request handlers
 sys.path.append(os.getcwd() + "/simpleTOS/requestHandlers/")
 
-from simpleTOS.code.ibmRequestHandler import * as ibm
-from simpleTOS.requestHandlers.summarizationRequestHandler import * as summary
-
+from ibmRequestHandler import *
+from summarizationRequestHandler import *
 """
 Important functions:
 IBM:
-ibm.parse_concepts("text") -- Replace text with the piece of text that you want to get the concepts for
+parse_concepts("text") -- Replace text with the piece of text that you want to get the concepts for
 
-Summary:
-summary.generate_summary(file_name, n) -- Generates summary of the given file and returns the top n lines(min: 1)
+Summary:W
+generate_summary(file_name, n) -- Generates summary of the given file and returns the top n lines(min: 1)
 """
 
+adobe_TOS = "../TOS-Examples/Adobe-Legal-Terms.txt"
 
+# generate_summary(adobe_TOS, 2)
