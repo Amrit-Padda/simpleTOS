f7d3aa8652caaa415920556d5ea973dae6956770
Jules Petit <jules.laptop@gmail.com>
2020-01-25 21:05:20 -0500

    Added the summarizer and conceptor

--- main/main.py
+++ main/main.py
@@ -3,6 +3,7 @@ import os
 
 from ibmRequestHandler import *
 from summarizationRequestHandler import *
+from webscraper import *
 """
 Important functions:
 IBM:
@@ -11,13 +12,13 @@ parse_concepts("text") -- Replace text with the piece of text that you want to g
 Summary:
 generate_summary(text, n) -- Generates summary of the given text and returns the top n lines(min: 1)
 """
+url = 'https://www.cgi.com/en/legal-restrictions'
+
 os.chdir(os.getcwd() + "/TOS-Examples")
 
-#conceptsSummarized = dict()
-#paragraphs = get_scraping_results() Would return an array of all web-scraped paragraphs
-#for paragraph in paragraphs:
- #   concept = parse_concepts(paragraph) #Get the main concept of the paragraph
- #   conceptsSummarized.update({concept[0] : generate_summary(paragraph, 2)}) #Add the concept and a summary of it to the list
-##Lambda expression to check for double new line
+conceptsSummarized = dict()
+paragraphs = scrape_data(url)# Would return an array of all web-scraped paragraphs
+for paragraph in paragraphs:
+    conceptsSummarized.update({parse_concepts(paragraph)[0] : generate_summary(paragraph, 2)}) #Add the concept and a summary of it to the list
 
 print(conceptsSummarized)
--- main/webscraper.py
+++ main/webscraper.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-CGI_URL = 'https://www.cgi.com/en/legal-restrictions'
-CGI_HTML = requests.get(CGI_URL).text
-CGI_SOUP = BeautifulSoup(CGI_HTML, 'html.parser')
-CGI_TEXT = CGI_SOUP.findAll("p", {"class": "align_left"})
+def scrape_data(url):
+    CGI_URL = url
+    CGI_HTML = requests.get(CGI_URL).text
+    CGI_SOUP = BeautifulSoup(CGI_HTML, 'html.parser')
+    CGI_TEXT = CGI_SOUP.findAll("p", {"class": "align_left"})
 
-print(CGI_TEXT)
+    return CGI_TEXT
