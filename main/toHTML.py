f79bf43b254800bb72c563a59304ef430331d9ab
Jules Petit <jules.laptop@gmail.com>
2020-01-25 22:30:31 -0500

    to HTML hopefully works

--- main/toHTML.py
+++ main/toHTML.py
@@ -17,5 +17,23 @@ import sys
 
 def to_html(summarizedConcepts):
     f = open("output.html", mode="w+")
+    f.write("<!DOCTYPE html>")
+    f.write("<html>")
+    f.write("<head>")
+    f.write("<meta charset=\"utf-8\">")
+    f.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\">")
+    f.write("<title>Output</title>")
+    f.write("</head>")
+    f.write("<body>")
     for key in summarizedConcepts.keys():
-        print(key)
+        if len(summarizedConcepts.get(key)) != 1: #1 Word aka what the header should be
+            f.write("<h1>" + str(summarizedConcepts.get(key) + "</h1>"))
+            f.write("<br>")
+        else:
+            f.write("<p1>" + str(summarizedConcepts.get(key)) + "</p1>")
+            f.write("<br>")
+    f.write("</body>")
+    f.write("</html>")
+
+
+
