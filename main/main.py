c98eb379e09872c57ff0b291828e76440f9009ae
Jules Petit <jules.laptop@gmail.com>
2020-01-26 09:01:54 -0500

    Ready to demo version
    We are only going to be using CGI, Shutterstock TOU and Shutterstock Licensces info for the demo

--- main/main.py
+++ main/main.py
@@ -14,16 +14,18 @@ Summary:
 generate_summary(text, n) -- Generates summary of the given text and returns the top n lines(min: 1)
 """
 cgi_url = 'https://www.cgi.com/en/legal-restrictions'
-st_url = 'https://www.shutterstock.com/terms'
+st_tou_url = 'https://www.shutterstock.com/terms'
+st_tos_url = 'https://www.shutterstock.com/license'
 
 os.chdir(os.getcwd() + "/TOS-Examples")
 
 conceptsSummarized = dict()
-paragraphs = scrape_data_SHUTTERSTOCK(st_url)# Would return an array of all web-scraped paragraphs
+paragraphs = scrape_data_SHUTTERSTOCK(st_tos_url)# Would return an array of all web-scraped paragraphs
 #paragraphs = scrape_data_CGI(cgi_url)
 # -4 is a hardcoded value configured for CGI's ToS
 for i in range(len(paragraphs) ):
     clean_paragraph = re.sub("\<(.*?)\>", "", paragraphs[i])
+    clean_paragraph = re.sub(r'[^\x00-\x7F]+',' " ', clean_paragraph)
 
     print("------------------")
     print(clean_paragraph)
@@ -39,9 +41,9 @@ for i in range(len(paragraphs) ):
     # Avoids overriding concept data when appending new info to it
     if concept in conceptsSummarized:
         if conceptsSummarized[concept].endswith('.'): #If the last character is a period, then just add space after
-            conceptsSummarized.update({concept : conceptsSummarized[concept] + "<br>" + (summary)})
+            conceptsSummarized.update({concept : conceptsSummarized[concept] + " <br><br>" + (summary)})
         else:
-            conceptsSummarized.update({concept : conceptsSummarized[concept] + ".<br>" + (summary)})
+            conceptsSummarized.update({concept : conceptsSummarized[concept] + ".<br><br>" + (summary)})
     else:
         conceptsSummarized.update({concept : summary}) #Add the concept and a summary of it to the list
 
